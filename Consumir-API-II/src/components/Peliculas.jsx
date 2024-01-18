cfef03c67bfa2eb5e8b65c44ef498b1cc024484c
Thrones <nestorcachacelmi.@gmail.com>
2024-01-17 22:31:16 -0500

    Dar estilos para Responsive

--- Consumir-API-II/src/components/Peliculas.jsx
+++ Consumir-API-II/src/components/Peliculas.jsx
@@ -1,8 +1,8 @@
 function ListaPeliculas({ peliculas }) {
 	return (
-		<ul>
+		<ul className='pelicula'>
 			{peliculas.map(peli => (
-				<li key={peli.id}>
+				<li className='pelicula' key={peli.id}>
 					<h3>{peli.title}</h3>
 					<p>{peli.year}</p>
 					<img src={peli.poster} alt={peli.title} />
